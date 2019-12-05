041a04db4f24ab4ab3f5029d38802a7051c8c245
KarinaSaraeava <karinasarava@gmail.com>
2019-12-05 15:38:58 +0700

    solution

---
+++ Common.cpp
@@ -0,0 +1,97 @@
+#include <string.h>
+#include <memory>
+#include <string>
+#include <iostream>
+using namespace std;
+class Expression {
+public:
+  virtual ~Expression() = default;
+  virtual int Evaluate() const = 0;
+  virtual std::string ToString() const = 0;
+};
+
+using ExpressionPtr = std::unique_ptr<Expression>;
+
+class ClassValue: public Expression{
+protected:
+int value;
+public:
+    ClassValue(int value){
+    this->value = value;
+    }
+    int Evaluate() const override{
+    return value;
+    }
+    string ToString() const override{
+        return('('+ to_string(value)+')');
+    }
+    ~ClassValue() override = default;
+};
+
+
+class ClassOperation: public Expression{
+protected:
+    ExpressionPtr left;
+    ExpressionPtr right;
+public:
+    ClassOperation(ExpressionPtr left_, ExpressionPtr right_){
+    left = move(left_);
+    right = move(right_);
+    };
+    virtual ~ClassOperation() override = default;
+    virtual int Evaluate() const override = 0;
+    virtual std::string ToString() const override = 0;
+};
+
+class ClassSum: public ClassOperation{
+
+public:
+    ClassSum(ExpressionPtr left_, ExpressionPtr right_) : ClassOperation(move(left_), move(right_)) {};
+    int Evaluate() const override{
+    return (left->Evaluate()+right->Evaluate());
+    }
+    string ToString() const override{
+        return('('+ (left->ToString())+'+'+(right->ToString())+')');
+    }
+    ~ClassSum() override = default;
+};
+
+class ClassProduct: public ClassOperation{
+
+public:
+    ClassProduct(ExpressionPtr left_, ExpressionPtr right_) : ClassOperation(move(left_), move(right_)) {};
+    int Evaluate() const override{
+    return (left->Evaluate()*right->Evaluate());
+    }
+    string ToString() const override{
+        return('('+ (left->ToString())+'*'+(right->ToString())+')');
+    }
+    ~ClassProduct() override = default;
+};
+
+// Функции для формирования выражения
+
+ExpressionPtr Value(int value){
+unique_ptr<ClassValue> ptr (new ClassValue(value));
+return ptr;
+};
+
+
+ExpressionPtr Sum(ExpressionPtr left, ExpressionPtr right){
+std::unique_ptr<ClassSum> ptr (new ClassSum(move(left), move(right)));
+return ptr;
+};
+
+ExpressionPtr Product(ExpressionPtr left, ExpressionPtr right){
+std::unique_ptr<ClassProduct> ptr (new ClassProduct(move(left), move(right)));
+return ptr;
+};
+
+
+int main(){
+
+ExpressionPtr ptr = Product(Sum(Value(9),Value(7)),Product(Value(5),Sum(Value(0),Value(5))));
+cout<<ptr->Evaluate()<< endl;
+cout<<ptr->ToString()<< endl;
+return 0;
+}
