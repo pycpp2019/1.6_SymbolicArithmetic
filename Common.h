add5d1390abc1a1d15f79421cf860f2a34c2eb4a
Dmitriev Maxim <example.email@example>
2019-12-18 22:23:46 +0700

    SymbolicArithmetic

--- Common.h
+++ Common.h
@@ -3,22 +3,22 @@
 #include <memory>
 #include <string>
 
-// Р‘Р°Р·РѕРІС‹Р№ РєР»Р°СЃСЃ Р°СЂРёС„РјРµС‚РёС‡РµСЃРєРѕРіРѕ РІС‹СЂР°Р¶РµРЅРёСЏ
+// Базовый класс арифметического выражения
 class Expression {
 public:
   virtual ~Expression() = default;
 
-  // Р’С‹С‡РёСЃР»СЏРµС‚ Р·РЅР°С‡РµРЅРёРµ РІС‹СЂР°Р¶РµРЅРёСЏ
+  // Вычисляет значение выражения
   virtual int Evaluate() const = 0;
 
-  // Р¤РѕСЂРјР°С‚РёСЂСѓРµС‚ РІС‹СЂР°Р¶РµРЅРёРµ РєР°Рє СЃС‚СЂРѕРєСѓ
-  // РљР°Р¶РґС‹Р№ СѓР·РµР» Р±РµСЂС‘С‚СЃСЏ РІ СЃРєРѕР±РєРё, РЅРµР·Р°РІРёСЃРёРјРѕ РѕС‚ РїСЂРёРѕСЂРёС‚РµС‚Р°
+  // Форматирует выражение как строку
+  // Каждый узел берётся в скобки, независимо от приоритета
   virtual std::string ToString() const = 0;
 };
 
 using ExpressionPtr = std::unique_ptr<Expression>;
 
-// Р¤СѓРЅРєС†РёРё РґР»СЏ С„РѕСЂРјРёСЂРѕРІР°РЅРёСЏ РІС‹СЂР°Р¶РµРЅРёСЏ
-ExpressionPtr Value(int value);
-ExpressionPtr Sum(ExpressionPtr left, ExpressionPtr right);
-ExpressionPtr Product(ExpressionPtr left, ExpressionPtr right);
+// Функции для формирования выражения
+ExpressionPtr Value(int val);
+ExpressionPtr Sum(ExpressionPtr e1, ExpressionPtr e2);
+ExpressionPtr Product(ExpressionPtr e1, ExpressionPtr e2);
